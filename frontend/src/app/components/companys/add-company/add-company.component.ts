b6d1d5d4e5ab544047dec085ea8b3ee96e63aa35
Janaina Militão <janaina.militao@gmail.com>
2024-01-04 20:59:50 -0300

    cadastro da instituição

--- frontend/src/app/components/companys/add-company/add-company.component.ts
+++ frontend/src/app/components/companys/add-company/add-company.component.ts
@@ -13,7 +13,7 @@ export class AddCompanyComponent {
 
   users: any[] = []; 
   formCompany: FormGroup; 
-  associate_company: any;
+
 
   ngOnInit() {
     this.service.getObjects("users/").subscribe(
@@ -25,8 +25,8 @@ export class AddCompanyComponent {
   company: Company = {
     id: 0,
     name: '',
-    associates_doc: [],
     guests: [],
+    associates_doc: [],
     user_created: '',
     date_updated: new Date(),
     date_creation: new Date(),
@@ -37,7 +37,6 @@ export class AddCompanyComponent {
     constructor(private service: ApiServive, private router: Router, private formBuilder: FormBuilder) {
       this.formCompany = this.formBuilder.group({
         name: ['', [Validators.required, Validators.maxLength(255)]],
-        associates_doc: [],
         guests: [],
         user_created: ['', Validators.required],
         language: [ 'pt', Validators.required],
@@ -47,10 +46,35 @@ export class AddCompanyComponent {
   
 
     addCompany(): void {
-      this.service.addObject("companys/",this.company).subscribe(() => {
-        this.router.navigate(['companys/']);
+      if (this.formCompany.valid) {
+        this.company.name = this.formCompany.get('name').value;
+        this.company.language = this.formCompany.get('language').value;
+        this.company.timezone = this.formCompany.get('timezone').value;
+        this.company.date_creation = new Date();
+        this.setUserCreated();
+        this.setGuests();
+        
+        this.service.addObject("companys/",this.company).subscribe(() => {
+          this.router.navigate(['companys/']);
+        });
+      }else{
+        console.log('Formulário inválido. Corrija os erros.');
+      }
+
+    }
+
+    setGuests(){
+      let list_id = this.formCompany.get('guests').value;
+      list_id.forEach((id) => {
+        this.company.guests.push(this.service.baseUrl+'users/'+id+'/')
       });
     }
+
+    setUserCreated(){
+      let user_id = this.formCompany.get('user_created').value;
+      this.company.user_created =  this.service.baseUrl+'users/'+user_id+'/'
+    }
+  
 
   }
     
